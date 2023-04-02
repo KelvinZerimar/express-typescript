3ae6c3e41ffa582d37a57bb4a766423f69fd4dbf
VSTS <kramirez27@outlook.es>
2023-04-02 23:39:15 +0200

    ADD: folder src and file index first endpoint API

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+const app = express();
+
+app.use(express.json())// middleware que transforma la req.body a un json
+
+const PORT = 3000
+
+app.get('/ping',(_req,res)=>{ // typescript ignora errores parametros con guión bajo
+    console.log('someone pinged here!!');
+    res.send('pong');
+});
+
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`);
+})
